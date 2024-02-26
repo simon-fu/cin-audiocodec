ef7cc888b5a7bffe68e296b6d6d72cc71bb685c8
simon-fu <simonok@139.com>
2024-02-26 11:41:15 +0800

    annexb_to_lengthed: support lengthed_to_annexb

--- annexb_to_lengthed/main.cpp
+++ annexb_to_lengthed/main.cpp
@@ -3,6 +3,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <vector>
+#include <algorithm>
 
 // #define ENABLE_DEBUG 1
 
@@ -14,20 +15,8 @@
 
 #define dbgi(...) printf(__VA_ARGS__)
 
-int main(int argc, char *argv[]) 
+int annexb_to_lengthed(const char * input, const char * output, int max_bytes)
 {
-   // const char * input = "/tmp/sample-data/test.h264";
-   // const char * output = "/tmp/test_lengthed.data";
-   int max_bytes = INT_MAX; // 10240;
-
-   if (argc != 3) {
-      dbgi("Usage: %s <input.h264> <output.data>\n", argv[0]);
-      return -1;
-   }
-
-   auto input = argv[1];
-   auto output = argv[2];
-
    AnnexBBuf annexbuf;
    FILE* ifile = NULL;
    FILE* ofile = NULL;
@@ -107,5 +96,129 @@ int main(int argc, char *argv[])
    }
 
    return 0;
+}
+
+int lengthed_to_annexb(const char * input, const char * output, int max_bytes)
+{
+   FILE* ifile = NULL;
+   FILE* ofile = NULL;
+   int num_units = 0;
+   int total_read_nbytes = 0;
+   const uint8_t ANNEXB[] = { 0, 0, 1 };
+
+   do
+   {
+      ifile = fopen(input, "rb");
+      if (!ifile) {
+         dbgi("Error opening file [%s]\n", input);
+         break;
+      }
+      dbgi("opened input [%s]\n", input);
+
+      ofile = fopen(output, "wb");
+      if (!ofile) {
+         dbgi("Error opening file [%s]\n", output);
+         break;
+      }
+      dbgi("opened output [%s]\n", output);
+
+
+      auto buf = std::vector<uint8_t>(4*1024);
+      int unit_len = 0;
+
+      while(feof(ifile) == 0 && total_read_nbytes < max_bytes) 
+      {
+         dbgd("====================\n");
+
+
+         {
+            int nbytes = fread(&unit_len, 1, 4, ifile);
+            if (nbytes < 4) 
+            {
+               break;
+            }
+            unit_len = htonl(unit_len);
+
+            total_read_nbytes += nbytes;
+            dbgd("read: wsize %d, bytes %d, total_read_nbytes %d\n", wsize, nbytes, total_read_nbytes);
+
+            fwrite(ANNEXB, 1, sizeof(ANNEXB)/sizeof(ANNEXB[0]), ofile);
+         }
+
+         if (unit_len > 0)
+         {
+            int num = 0;
+            while (num < unit_len) 
+            {
+               auto remains = (unit_len - num);
+               auto chunk_len = std::min((int)buf.size(), remains);
+               int nbytes = fread(buf.data(), 1, chunk_len, ifile);
+               if (nbytes > 0) 
+               {
+                  fwrite(buf.data(), 1, chunk_len, ofile);
+               }
+               num += chunk_len;
+            }
+
+            total_read_nbytes += unit_len;
+            ++num_units;
+         }
+      }
+   } while (0);
+   
+   dbgi("read: bytes %d, unit %d\n", total_read_nbytes, num_units);
+
+   if (ifile)
+   {
+      fclose(ifile);
+      ifile = NULL;
+   }
+
+   if (ofile)
+   {
+      fclose(ofile);
+      ofile = NULL;
+   }
+
+   return 0;
+}
+
+// static int test()
+// {
+//    const char * output = "/tmp/test_lengthed.data";
+//    const int max_bytes = INT_MAX;
+//    int ret = annexb_to_lengthed
+//    (
+//       "/tmp/sample-data/test.h264", 
+//       output, 
+//       max_bytes
+//    );
+
+//    if (ret == 0)
+//    {
+//       ret = lengthed_to_annexb
+//       (
+//          output,
+//          "/tmp/test_lengthed.h264", 
+//          max_bytes
+//       );
+//    }
+   
+//    return ret;
+// }
+
+int main(int argc, char *argv[]) 
+{   
+   // return test();
+
+   if (argc != 3) {
+      dbgi("Usage: %s <input.h264> <output.data>\n", argv[0]);
+      return -1;
+   }
+
+   auto input = argv[1];
+   auto output = argv[2];
+
+   return annexb_to_lengthed(argv[1], argv[2], INT_MAX);
 }
 
